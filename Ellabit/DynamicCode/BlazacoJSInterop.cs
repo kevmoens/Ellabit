05ebc78d46a0a1da6dc61c62700f63b212164a1d
Kevin <kmoens@lakeco.com>
2022-06-13 22:25:26 -0500

    Add monaco editor (VS Code Editor)

---
+++ Ellabit/DynamicCode/BlazacoJSInterop.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Ellabit.DynamicCode
+{
+    public static class BlazacoJSInterop
+    {
+        public static ValueTask<bool> InitializeEditor(IJSRuntime runtime, string editorModel)
+            => runtime.InvokeAsync<bool>("Blazaco.Editor.InitializeEditor", new[] { editorModel });
+
+        public static ValueTask<string> GetValue(IJSRuntime runtime, string id)
+            => runtime.InvokeAsync<string>("Blazaco.Editor.GetValue", new[] { id });
+
+        public static ValueTask<bool> SetValue(IJSRuntime runtime, string id, string value)
+            => runtime.InvokeAsync<bool>("Blazaco.Editor.SetValue", new[] { id, value });
+
+        public static ValueTask<bool> SetTheme(IJSRuntime runtime, string id, string theme)
+            => runtime.InvokeAsync<bool>("Blazaco.Editor.SetTheme", new[] { id, theme });
+    }
+}
---
+++ Ellabit/wwwroot/scripts/monaco.js
@@ -0,0 +1,53 @@
+﻿window.Blazaco = window.Blazaco || {};
+window.Blazaco.Editors = [];
+
+window.Blazaco.Editor = {
+    InitializeEditor: function (editorId) {
+        let thisEditor = monaco.editor.create(document.getElementById(editorId), {
+            value: ['void Main() {', '\tConsole.WriteLine("Hello world!");', '}'].join('\n'),
+            language: 'csharp'
+        });
+        if (window.Blazaco.Editors.find(e => e.id === editorId)) {
+            return false;
+        }
+        else {
+            window.Blazaco.Editors.push({ id: editorId, editor: thisEditor });
+        }
+        return true;
+    },
+    GetValue: function (id) {
+        let myEditor = window.Blazaco.Editors.find(e => e.id === id);
+        if (!myEditor) {
+            throw `Could not find a editor with id: '${window.Blazaco.Editors.length}' '${id}'`;
+        }
+        return myEditor.editor.getValue();
+    },
+    SetValue: function (id, value) {
+        let myEditor = window.Blazaco.Editors.find(e => e.id === id);
+        if (!myEditor) {
+            throw `Could not find a editor with id: '${window.Blazaco.Editors.length}' '${id}'`;
+        }
+        myEditor.editor.setValue(value);
+        return true;
+    },
+    SetTheme: function (id, theme) {
+        let myEditor = window.Blazaco.Editors.find(e => e.id === id);
+        if (!myEditor) {
+            throw `Could not find a editor with id: '${window.Blazaco.Editors.length}' '${id}'`;
+        }
+        monaco.editor.setTheme(theme);
+        return true;
+    }
+}
+
+//function initEditor() {
+//    require.config({ paths: { vs: 'monaco-editor/min/vs' } });
+//    require(['vs/editor/editor.main'], function () {
+//        var editor = monaco.editor.create(document.getElementById('editor'), {
+//            value: ['void Main() {', '\tConsole.WriteLine("Hello world!");', '}'].join('\n'),
+//            language: 'csharp'
+//        });
+        
+//    });
+//}
+
