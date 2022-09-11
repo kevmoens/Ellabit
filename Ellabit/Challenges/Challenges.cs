63adb5e6cb41e3b27faacbdd662ea05c8c769d92
Kevin <kmoens@lakeco.com>
2022-09-10 18:51:19 -0600

    new challenge

--- Ellabit/Challenges/Challenges.cs
+++ Ellabit/Challenges/Challenges.cs
@@ -174,6 +174,7 @@
             challenges.Add(new Challenge175Countonesinbinaryrepresentationofinteger());
             challenges.Add(new Challenge176Fractiongreaterthanone());
             challenges.Add(new Challenge177Positivecountnegativesum());
+            challenges.Add(new Challenge178Returnthesumofthetwosmallestnumbers());
             challenges.Add(new ChallengeColor001());
             return challenges;
         }
