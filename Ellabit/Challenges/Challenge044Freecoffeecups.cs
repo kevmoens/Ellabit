df5352a189be72d32c18e7a8d873aedffdc41fdc
Kevin <kmoens@lakeco.com>
2022-10-10 18:04:19 -0600

    Allow blockly on more challenges

--- Ellabit/Challenges/Challenge044Freecoffeecups.cs
+++ Ellabit/Challenges/Challenge044Freecoffeecups.cs
@@ -84,7 +84,7 @@ only valid inputs will be given.";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase) { { "Math", "Multiplication" }, {"Math:2", "Modulo" }, { "Level", "2" } };
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
-        public bool ShowBlockly { get => false; }
+        public bool ShowBlockly { get => true; }
         public string BlocklyXML { get => string.Empty; }
         public string BlocklyToolboxXML { get => string.Empty; }
     }
