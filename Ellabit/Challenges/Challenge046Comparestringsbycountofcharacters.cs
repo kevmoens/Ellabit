92453fd0e7321d30a30c6a6e16dea4c5471bb44f
Kevin <kmoens@lakeco.com>
2022-10-14 18:04:19 -0600

    Allow blockly on more challenges

--- Ellabit/Challenges/Challenge046Comparestringsbycountofcharacters.cs
+++ Ellabit/Challenges/Challenge046Comparestringsbycountofcharacters.cs
@@ -82,7 +82,7 @@ Don't forget to return the result.";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
-        public bool ShowBlockly { get => false; }
+        public bool ShowBlockly { get => true; }
         public string BlocklyXML { get => string.Empty; }
         public string BlocklyToolboxXML { get => string.Empty; }
     }
