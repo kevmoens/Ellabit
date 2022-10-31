4c0a6d857d639af3226e88d937a047f6a97ec29b
Kevin <kmoens@lakeco.com>
2022-10-30 18:04:19 -0600

    add new test

--- Ellabit/Challenges/Challenge194Sortnumbersindescendingorder.cs
+++ Ellabit/Challenges/Challenge194Sortnumbersindescendingorder.cs
@@ -1,20 +1,21 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge193Formatnumberwithcommaseparatingthousands : IChallenge
+    public class Challenge194Sortnumbersindescendingorder : IChallenge
     {
-        public string? Header { get; set; } = "Format number with comma(s) separating thousands";
+        public string? Header { get; set; } = "Sort numbers in descending order";
         public string? Code { get; set; } = @"
 using System;
 
 namespace Ellabit;
 
-public class Challenge
+public class Challenge 
 {
-    public  string FormatNum(int num)
+    public  int SortDescending(int num) 
     {
-			
+      
     }
 }
+
 
 ";
         public string? TestCode { get; set; } = @"
@@ -27,57 +28,58 @@ public class TestChallenge
     public (bool pass, string message) Test1()
     {
         var tmp = new Challenge();
-        string sumResult;
+        int sumResult;
         try 
         {
-            sumResult = tmp.FormatNum(1000)  ;
+            sumResult = tmp.SortDescending(123)  ;
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult ==   "" 1,000"" ,  $""returned: {sumResult}  expected:  1,000"");
+        return (sumResult ==   321,  $""returned: {sumResult}  expected: 321"");
     }
     public (bool pass, string message) Test2()
     {
         var tmp = new Challenge();
-        string sumResult;
+        int sumResult;
         try 
         {
-            sumResult = tmp.FormatNum(100000) ;
+            sumResult = tmp.SortDescending(2619805)  ;
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult ==  ""100,000"" ,   $""returned: {sumResult}  expected: 100,000"");
+        return (sumResult ==   9865210,   $""returned: {sumResult}  expected: 9865210"");
     }
     public (bool pass, string message) Test3()
     {
         var tmp = new Challenge();
-        string sumResult;
+        int sumResult;
         try 
         {
-            sumResult = tmp.FormatNum(20);
+            sumResult = tmp.SortDescending(73065);
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + ""\n"" + ex.Message);
         }
-        return (sumResult == 20,   $""returned: {sumResult}  expected: 20"");
+        return (sumResult == 76530,   $""returned: {sumResult}  expected: 76530"");
     }
 }
 ";
-        public string? Description { get; set; } = @"Create a function that takes a number as an argument and returns a string formatted to separate thousands.
+        public string? Description { get; set; } = @"Create content  a  function  that  takes  any  nonnegative  number  as  an  argument  and  return  it  with  it's  digits  in  descending  order.  descending  order  is  when  you  sort  from  highest  to  lowest.
 
         <h5>Examples</h5>
         <code>
-FormatNum(1000) ➞ ""1,000""
+sortdescending(123)  ➞  321
 
-FormatNum(100000) ➞ ""100,000""
+sortdescending(2619805)  ➞  9865210
 
-FormatNum(20) ➞ ""20""
+sortdescending(73065)  ➞  76530
             </p>
         </code>
-Notes
-You can expect a valid number for all test cases.";
+
+notes
+you  can  expect  non-negative  numbers  for  all  test  cases.""";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         public Dictionary<string, string> Tags { get; set; } = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
     }
