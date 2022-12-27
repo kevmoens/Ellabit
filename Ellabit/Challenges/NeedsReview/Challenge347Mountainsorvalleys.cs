746dd9a13884bb8fd150444d76ee78a2875f30e0
Kevin <kmoens@lakeco.com>
2022-12-26 18:04:19 -0600

    fix descriptions

--- Ellabit/Challenges/NeedsReview/Challenge347Mountainsorvalleys.cs
+++ Ellabit/Challenges/NeedsReview/Challenge347Mountainsorvalleys.cs
@@ -1,4 +1,4 @@
-namespace Ellabit.Challenges
+﻿namespace Ellabit.Challenges
 {
     public class Challenge347Mountainsorvalleys : IChallenge
     {
@@ -65,17 +65,41 @@ public class TestChallenge
     }
 }
 ";
-        public string? Description { get; set; } = @"A content  mountain  is  an  array  with  exactly  one  peak.
+        public string? Description { get; set; } = @"A mountain is an array with exactly one peak.
 
-all  numbers  to  the  left  of  the  peak  are  increasing.
-all  numbers  to  the  right  of  the  peak  are  decreasing.
-the  peak  cannot  be  on  the  boundary.
+All numbers to the left of the peak are increasing.
+All numbers to the right of the peak are decreasing.
+The peak CANNOT be on the boundary.
+A valley is an array with exactly one trough.
 
-a  valley  is  an  array  with  exactly  one  trough.
+All numbers to the left of the trough are decreasing.
+All numbers to the right of the trough are increasing.
+The trough CANNOT be on the boundary.
+Some examples of mountains and valleys:
 
-all  numbers  to  the  left  of  the  trough  are  decreasing.
-all  numbers  to  the  right  of  the  trough  are  increasing.
-the  trough  cannot""";
+Mountain A:  [1, 3, 5, 4, 3, 2]   // 5 is the peak
+Mountain B:  [-1, 0, -1]   // 0 is the peak
+Mountain B:  [-1, -1, 0, -1, -1]   // 0 is the peak (plateau on both sides is okay)
+
+Valley A: [10, 9, 8, 7, 2, 3, 4, 5]   // 2 is the trough
+Valley B: [350, 100, 200, 400, 700]  // 100 is the trough
+Neither mountains nor valleys:
+
+Landscape A: [1, 2, 3, 2, 4, 1]  // 2 peaks (3, 4), not 1
+Landscape B: [5, 4, 3, 2, 1]  // Peak cannot be a boundary element
+Landscape B: [0, -1, -1, 0, -1, -1]  // 2 peaks (0)
+Based on the rules above, write a function that takes in an array and returns either ""mountain"", ""valley"", or ""neither"".
+
+Examples
+landscapeType([3, 4, 5, 4, 3]) ➞ ""mountain""
+
+landscapeType([9, 7, 3, 1, 2, 4]) ➞ ""valley""
+
+landscapeType([9, 8, 9]) ➞ ""valley""
+
+landscapeType([9, 8, 9, 8]) ➞ ""neither""
+Notes
+A peak is not exactly the same as the max of an array. The max is a unique number, but an array may have multiple peaks. However, if there exists only one peak in an array, then it is true that the peak = max.";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
--- Ellabit/Challenges/NeedsReview/Challenge348Leastcommonmultiple.cs
+++ Ellabit/Challenges/NeedsReview/Challenge348Leastcommonmultiple.cs
@@ -1,21 +1,29 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge346Excelsheetcolumnnumber : IChallenge
+    public class Challenge348Leastcommonmultiple : IChallenge
     {
-        public string? Header { get; set; } = "Excel sheet column number";
+        public string? Header { get; set; } = "Least common multiple";
         public string? Code { get; set; } = @"
 using System;
 
 namespace Ellabit;
 
+// Its possible to solve using some or all of these librarys.
+
 using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Challenge
 {
-    public  int TitleToNumber(string str) {
-	  	return 0;
-	}	
-}
+	public  int LCM(int[] nums)
+	{
+		return 0;
+	}
+}	
 
 ";
         public string? TestCode { get; set; } = @"
@@ -31,12 +39,14 @@ public class TestChallenge
         int sumResult;
         try 
         {
-            sumResult = tmp.<rep.test1>;
+            sumResult = tmp.lcm({  1,  2,  3,  4,  5,  6,  7,  8,  9  })  ;
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult == <rep.test.result1>,  $""returned: {sumResult}  expected: <rep.test.result1Val>"");
+        return (sumResult ==   2520
+
+,  $""returned: {sumResult}  expected: 2520"");
     }
     public (bool pass, string message) Test2()
     {
@@ -44,12 +54,14 @@ public class TestChallenge
         int sumResult;
         try 
         {
-            sumResult = tmp.<rep.test2>;
+            sumResult = tmp.lcm({  5  })  ;
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult == <rep.test.result2>,   $""returned: {sumResult}  expected: <rep.test.result2Val>"");
+        return (sumResult ==   5
+
+,   $""returned: {sumResult}  expected: 5"");
     }
     public (bool pass, string message) Test3()
     {
@@ -57,20 +69,28 @@ public class TestChallenge
         int sumResult;
         try 
         {
-            sumResult = tmp.<rep.test3>;
+            sumResult = tmp.lcm({  5,  7,  11  })  ;
         } catch (Exception ex) 
         {
             return (false, ex.ToString() + ""\n"" + ex.Message);
         }
-        return (sumResult == <rep.test.result3>,   $""returned: {sumResult}  expected: <rep.test.result3Val>"");
+        return (sumResult ==   385
+
+,   $""returned: {sumResult}  expected: 385"");
     }
 }
 ";
-        public string? Description { get; set; } = @"Given content  a  column  title  as  it  appears  in  an  excel  sheet  return  its  corresponding  column  number.
+        public string? Description { get; set; } = @"Given content  a  array  of  integers,  create  a  function  that  will  find  the  smallest  positive  integer  that  is  evenly  divisible  by  all  the  members  of  the  list.  in  other  words,  find  the  least  common  multiple  (lcm).
 
-the  number  is  computed  in  the  following  way:
+examples
+lcm({  1,  2,  3,  4,  5,  6,  7,  8,  9  })  ➞  2520
 
-a";
+lcm({  5  })  ➞  5
+
+lcm({  5,  7,  11  })  ➞  385
+
+lcm({  5,  7,  11,  35,  55,  77  })  ➞  385
+";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
