0b69a57c89d6b4fa03dc299ae7b920cf653c7516
Kevin <kmoens@lakeco.com>
2022-12-28 18:04:19 -0600

    fix descriptions

--- Ellabit/Challenges/NeedsReview/Challenge351Longestalternatingsubstring.cs
+++ Ellabit/Challenges/NeedsReview/Challenge351Longestalternatingsubstring.cs
@@ -65,10 +65,20 @@ public class TestChallenge
     }
 }
 ";
-        public string? Description { get; set; } = @"Given content  a  string  of  digits,  return  the  longest  substring  with  alternating  odd/even  or  even/odd  digits.  if  two  or  more  substrings  have  the  same  length,  return  the  substring  that  occurs  first.
+        public string? Description { get; set; } = @"Given a string of digits, return the longest substring with alternating odd/even or even/odd digits. If two or more substrings have the same length, return the substring that occurs first.
 
-examples
-longestsubstring("" 225424272163254474441338664823"") ➞ ""272163254"" ""254,"" substrings 272163254, 474, 41, 38, 23 longestsubstring(""594127169973391692147228678476""""";
+Examples
+longestSubstring(""225424272163254474441338664823"") ➞ ""272163254""
+// substrings = 254, 272163254, 474, 41, 38, 23
+
+longestSubstring(""594127169973391692147228678476"") ➞ ""16921472""
+// substrings = 94127, 169, 16921472, 678, 476
+
+longestSubstring(""721449827599186159274227324466"") ➞ ""7214""
+// substrings = 7214, 498, 27, 18, 61, 9274, 27, 32
+// 7214 and 9274 have same length, but 7214 occurs first.
+Notes
+The minimum alternating substring size is 2, and there will always be at least one alternating substring.";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
--- Ellabit/Challenges/NeedsReview/Challenge352Powerfulprimefactor.cs
+++ Ellabit/Challenges/NeedsReview/Challenge352Powerfulprimefactor.cs
@@ -67,10 +67,23 @@ public class TestChallenge
     }
 }
 ";
-        public string? Description { get; set; } = @"Create content  a  function  that  takes  a  positive  integer  and  returns  a  string  expressing  how  the  number  can  be  made  by  multiplying  powers  of  its  prime  factors.
+        public string? Description { get; set; } = @"Create a function that takes a positive integer and returns a string expressing how the number can be made by multiplying powers of its prime factors.
 
-examples
-expressfactors(2)  ➞  "" 2"" expressfactors(4) ➞ ""2^2"" expressfactors(10) ""2 x 5"" expressfactors(60) ""2^2 3 notes all inputs will be positive integers in the range 1 < n 10,000. if a factor""";
+Examples
+expressFactors(2) ➞ ""2""
+
+expressFactors(4) ➞ ""2^2""
+
+expressFactors(10) ➞ ""2 x 5""
+
+expressFactors(60) ➞ ""2^2 x 3 x 5""
+Notes
+All inputs will be positive integers in the range 1 < n < 10,000.
+If a factor is repeated express it in the form ""x^y"" where x is the factor and y is the number of repetitions of the factor.
+If n is a prime number, return n as a string as in example #1 above.
+Factors should appear in ascending order in the expression.
+Make sure you include the space either side of the multiplication sign, "" x "".
+Check the Resources if you need to understand Prime Factorization.";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
