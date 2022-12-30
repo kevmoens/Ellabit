23b59f92f3ddcadc8971a6d23312d2352cc6363c
Kevin <kmoens@lakeco.com>
2022-12-29 18:04:19 -0600

    fix TestChallenge

--- Ellabit/Challenges/NeedsReview/Challenge198Findingnemo.cs
+++ Ellabit/Challenges/NeedsReview/Challenge198Findingnemo.cs
@@ -1,12 +1,14 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge198Findingnemo : IChallenge
+    public class Challenge198FindingNemo : IChallenge, IChallengeTestCode
     {
         public string? Header { get; set; } = "Finding nemo";
         public string? Code { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+{
+
 
 using System;
 class Program
@@ -17,11 +19,14 @@ class Program
 	}
 }
 
+
+}
 ";
         public string? TestCode { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+    {
 
 public class TestChallenge
 {
@@ -31,12 +36,12 @@ public class TestChallenge
         string sumResult;
         try 
         {
-            sumResult = tmp.findnemo(""i am finding !"") ;
+                sumResult = tmp.FindNemo(""I am finding Nemo !"");
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + "" "" + ex.Message);
+                return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult ==  4!"" ,  $""returned: {sumResult}  expected: 4!"");
+            return (sumResult == ""I found Nemo at 4!"",  $""returned: {sumResult}  expected: I found Nemo at 4!"");
     }
     public (bool pass, string message) Test2()
     {
@@ -44,12 +49,12 @@ public class TestChallenge
         string sumResult;
         try 
         {
-            sumResult = tmp.<rep.test2>;
+                sumResult = tmp.FindNemo(""Nemo is me"");
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + "" "" + ex.Message);
+                return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult == <rep.test.result2>,   $""returned: {sumResult}  expected: <rep.test.result2Val>"");
+            return (sumResult == ""I found Nemo at 1!"",  $""returned: {sumResult}  expected: I found Nemo at 1!"");
     }
     public (bool pass, string message) Test3()
     {
@@ -57,30 +62,21 @@ public class TestChallenge
         string sumResult;
         try 
         {
-            sumResult = tmp.<rep.test3>;
+                sumResult = tmp.FindNemo(""I Nemo am"");
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + ""\n"" + ex.Message);
+                return (false, ex.ToString() + "" "" + ex.Message);
+            }
+            return (sumResult == ""I found Nemo at 2!"",  $""returned: {sumResult}  expected: I found Nemo at 2!"");
         }
-        return (sumResult == <rep.test.result3>,   $""returned: {sumResult}  expected: <rep.test.result3Val>"");
     }
 }
 ";
-        public string? Description { get; set; } = @"You're given a string of words. You need to find the word ""Nemo"", and return a string like this: ""I found Nemo at [the order of the word you find Nemo]!"".
-
-If you can't find Nemo, return ""I can't find Nemo :("".
-
-Examples
-FindNemo(""I am finding Nemo !"") ➞ ""I found Nemo at 4!""
+        public string? Description { get; set; } = @"<div><p><span>You're given a string of words. You need to find the word ""Nemo"", and return a string like this: </span><code>""I found Nemo at [the order of the word you find Nemo]!""</code><span>.</span></p><p><span>If you can't find Nemo, return </span><code>""I can't find Nemo :(""</code><span>.</span></p><h3><span>Examples</span></h3><pre><code>FindNemo(""I am finding Nemo !"") ➞ ""I found Nemo at 4!""
 
 FindNemo(""Nemo is me"") ➞ ""I found Nemo at 1!""
 
-FindNemo(""I Nemo am"") ➞ ""I found Nemo at 2!""
-Notes
-! , ? . are always separated from the last word.
-Nemo will always look like Nemo, and not NeMo or other capital variations.
-Nemo's, or anything that says Nemo with something behind it, doesn't count as Finding Nemo.
-If there are multiple Nemo's in the sentence, only return the first one.";
+FindNemo(""I Nemo am"") ➞ ""I found Nemo at 2!""</code></pre><h3><span>Notes</span></h3><ul><li><code>! , ? .</code><span> are always separated from the last word.</span></li><li><span>Nemo will always look like </span><em><span>Nemo</span></em><span>, and not </span><em><span>NeMo</span></em><span> or other capital variations.</span></li><li><em><span>Nemo's</span></em><span>, or anything that says </span><em><span>Nemo</span></em><span> with something behind it, doesn't count as </span><em><span>Finding Nemo</span></em><span>.</span></li><li><span>If there are multiple </span><em><span>Nemo's</span></em><span> in the sentence, only return the first one.</span></li></ul></div>";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
         public Dictionary<string, string> Tags { get; set; } = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
     }
