3ff662162a4eaf479d147287ae19aa62dfbb7e51
Kevin <kmoens@lakeco.com>
2022-12-31 18:04:19 -0600

    fix TestChallenge

--- Ellabit/Challenges/NeedsReview/Challenge201Pitondecimalplaces.cs
+++ Ellabit/Challenges/NeedsReview/Challenge201Pitondecimalplaces.cs
@@ -1,12 +1,14 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge201Pitondecimalplaces : IChallenge
+    public class Challenge201PitoNDecimalPlaces : IChallenge, IChallengeTestCode
     {
         public string? Header { get; set; } = "Pi to n decimal places";
         public string? Code { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+{
+
 
 public class Challenge 
 {
@@ -16,11 +18,14 @@ public class Challenge
     }
 }
 
+
+}
 ";
         public string? TestCode { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+    {
 
 public class TestChallenge
 {
@@ -35,9 +40,7 @@ public class TestChallenge
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult ==   3.14159
-
-,  $""returned: {sumResult}  expected: 3.14159"");
+            return (sumResult == 3.14159,  $""returned: {sumResult}  expected: 3.14159"");
     }
     public (bool pass, string message) Test2()
     {
@@ -50,9 +53,7 @@ public class TestChallenge
         {
             return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult ==   3.1416
-
-,   $""returned: {sumResult}  expected: 3.1416"");
+            return (sumResult == 3.1416,  $""returned: {sumResult}  expected: 3.1416"");
     }
     public (bool pass, string message) Test3()
     {
@@ -63,28 +64,21 @@ public class TestChallenge
             sumResult = tmp.mypi(15)  ;
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + ""\n"" + ex.Message);
+                return (false, ex.ToString() + "" "" + ex.Message);
+            }
+            return (sumResult == 3.141592653589793,  $""returned: {sumResult}  expected: 3.141592653589793"");
         }
-        return (sumResult ==   3.141592653589793
-
-,   $""returned: {sumResult}  expected: 3.141592653589793"");
     }
 }
 ";
-        public string? Description { get; set; } = @"Given content  a  number  n,  write  a  function  that  returns  pi  to  n  decimal  places.
-
-examples
-mypi(5)  ➞  3.14159
+        public string? Description { get; set; } = @"<div><p><span>Given a number </span><code>n</code><span>, write a function that returns PI to </span><code>n</code><span> decimal places.</span></p><h3><span>Examples</span></h3><pre><code>MyPi(5) ➞ 3.14159
 
 mypi(4)  ➞  3.1416
 
-mypi(15)  ➞  3.141592653589793
-
-notes
-n  will  not  be  above  15,  to  keep  this  challenge  simple.
-round  up  the  last  digit  if  the  next  digit  in  pi  is  greater  or  equal  to  5  (see  second  example  above).
-the  return  value  must  be  a  number  (add  suffix  -m  t""";
+MyPi(15) ➞ 3.141592653589793</code></pre><h3><span>Notes</span></h3><ul><li><code>n</code><span> will not be above 15, to keep this challenge simple.</span></li><li><span>Round up the last digit if the next digit in PI is greater or equal to 5 (see second example above).</span></li><li><span>The return value must be a number (add  suffix -m to returning number), not a string.</span></li></ul></div>";
         public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
-        public Dictionary<string, string> Tags { get; set; } = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+        private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
     }
 }
