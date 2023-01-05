18ee488eb1b82da374aecbe9e07d27f442746732
Kevin <kmoens@lakeco.com>
2023-01-04 18:04:19 -0600

    edit description of Test

--- Ellabit/Challenges/NeedsReview/DescriptionReview/Challenge353Findmissingalphabets.cs
+++ Ellabit/Challenges/NeedsReview/DescriptionReview/Challenge353Findmissingalphabets.cs
@@ -1,12 +1,13 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge353Findmissingalphabets : IChallenge
+    public class Challenge353FindMissingAlphabets : IChallenge, IChallengeTestCode
     {
         public string? Header { get; set; } = "Find missing alphabets";
         public string? Code { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+{
 
 using System;
 
@@ -17,11 +18,14 @@ public class Challenge
 	}
 }
 
+
+}
 ";
         public string? TestCode { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+    {
 
 public class TestChallenge
 {
@@ -65,14 +69,17 @@ public class TestChallenge
         return (sumResult == <rep.test.result3>,   $""returned: {sumResult}  expected: <rep.test.result3Val>"");
     }
 }
+}
 ";
-        public string? Description { get; set; } = @"Create content  a  function  that  takes  a  string  str  containing  only  letters  from  a  to  z  in  lowercase  and  returns  the  missing  letter(s)  in  alphabetical  order  a-z.
-a  set  of  letters  is  given  by  abcdefghijklmnopqrstuvwxyz.
-two  sets  of  alphabets  means  two  or  more  alphabets.
+        public string? Description { get; set; } = @"<div><p><span>Create a function that takes a string </span><code>str</code><span> containing only letters from </span><strong><span>a to z</span></strong><span> in lowercase and returns the </span><strong><span>missing letter(s)</span></strong><span> in alphabetical order a-z.</span></p><ul><li><span>A set of letters is given by </span><code>abcdefghijklmnopqrstuvwxyz</code><span>.</span></li><li><span>Two sets of alphabets means two or more alphabets.</span></li></ul><h3><span>Examples</span></h3><pre><code>MissingAlphabets(""abcdefghijklmnopqrstuvwxy"") ➞ ""z""
+// ""z"" is missing.
 
-examples
-missingalphabets("" abcdefghijklmnopqrstuvwxy"") ➞ ""z"" is missing. missingalphabets(""aa""";
-        public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
+MissingAlphabets(""aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyy"") ➞ ""zz""
+// Given string has a set of two alphabets so output will be ""zz""
+
+MissingAlphabets(""edabit"") ➞ ""cfghjklmnopqrsuvwxyz""</code></pre><h3><span>Notes</span></h3><p><span>If the string contains all letters from </span><code>a-z</code><span>, return an empty string </span><code>""""</code><span>.</span></p></div>";
+        public List<string> Tests { get; set; } = new string[] { "Test1", "Test2" }.ToList();
+
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
     }
