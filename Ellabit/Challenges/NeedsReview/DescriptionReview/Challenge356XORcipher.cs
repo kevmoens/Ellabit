8afab93ce536bacb1d9c7ec4d7ac16aa11817fa0
Kevin <kmoens@lakeco.com>
2023-01-07 18:04:19 -0600

    edit description of Test

--- Ellabit/Challenges/NeedsReview/DescriptionReview/Challenge356XORcipher.cs
+++ Ellabit/Challenges/NeedsReview/DescriptionReview/Challenge356XORcipher.cs
@@ -1,14 +1,14 @@
 namespace Ellabit.Challenges
 {
-    public class Challenge356XORcipher : IChallenge
+    public class Challenge356XORCipher : IChallenge, IChallengeTestCode
     {
         public string? Header { get; set; } = "XOR cipher";
         public string? Code { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+{
 
-using System;
 
 public class Challenge 
 {
@@ -18,11 +18,14 @@ public class Challenge
 	}
 }
 
+
+}
 ";
         public string? TestCode { get; set; } = @"
 using System;
 
-namespace Ellabit;
+namespace Ellabit
+    {
 
 public class TestChallenge
 {
@@ -32,48 +35,38 @@ public class TestChallenge
         string sumResult;
         try 
         {
-            sumResult = tmp.<rep.test1>;
+                sumResult = tmp.XORCipher(""11"" ""22"");
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + "" "" + ex.Message);
+                return (false, ex.ToString() + "" "" + ex.Message);
         }
-        return (sumResult == <rep.test.result1>,  $""returned: {sumResult}  expected: <rep.test.result1Val>"");
+            return (sumResult == ""33"",  $""returned: {sumResult}  expected: 33"");
     }
-    public (bool pass, string message) Test2()
+
+        public (bool pass, string message) <rep.test.testname>()
     {
         var tmp = new Challenge();
-        string sumResult;
+            <rep.test.returnType> sumResult;
         try 
         {
-            sumResult = tmp.<rep.test2>;
+                sumResult = tmp.<rep.test>;
         } catch (Exception ex) 
         {
-            return (false, ex.ToString() + "" "" + ex.Message);
-        }
-        return (sumResult == <rep.test.result2>,   $""returned: {sumResult}  expected: <rep.test.result2Val>"");
+                return (false, ex.ToString() + "" "" + ex.Message);
     }
-    public (bool pass, string message) Test3()
-    {
-        var tmp = new Challenge();
-        string sumResult;
-        try 
-        {
-            sumResult = tmp.<rep.test3>;
-        } catch (Exception ex) 
-        {
-            return (false, ex.ToString() + ""\n"" + ex.Message);
+            return (<rep.test.resultCheck>,  $""returned: <rep.test.resultDisplay>  expected: <rep.test.resultVal>"");
         }
-        return (sumResult == <rep.test.result3>,   $""returned: {sumResult}  expected: <rep.test.result3Val>"");
     }
 }
 ";
-        public string? Description { get; set; } = @"In content  xor  cipher,  encoding  is  done  by  an  xor  operation  on  two  given  strings.  if  the  strings  are  of  different  lengths  then  the  output  should  be  the  length  of  the  shorter  string.  cut  the  length  of  the  longer  string  to  the  same  size  as  the  smaller  string  before  xor  operation.
+        public string? Description { get; set; } = @"<div><p><span>In </span><strong><span>XOR Cipher</span></strong><span>, encoding is done by an XOR operation on two given strings. If the strings are of different lengths then the output should be the length of the shorter string. Cut the length of the longer string to the same size as the smaller string before XOR operation.</span></p><p><span>Given two strings of </span><code>msg1</code><span> and </span><code>msg2</code><span>, return the </span><strong><span>encoded message</span></strong><span>.</span></p><h3><span>Examples</span></h3><pre><code>XORCipher(""11"", ""22"") ➞ ""33""
+// 11 XOR 22 = 33
 
-given  two  strings  of  msg1  and  msg2,  return  the  encoded  message.
+XORCipher(""1020304"", ""403201"") ➞ ""501231""
 
-examples
-xorcipher("" 11"", """;
-        public List<string> Tests { get; set; } = new string[] { "Test1", "Test2", "Test3" }.ToList();
+XORCipher(""c611d9bdd9de38b9eb"", ""23a0745505d4d25494"") ➞ ""e5b1ade8dc0aeaed7f""</code></pre><h3><span>Notes</span></h3><p><span>A hint is in the </span><strong><span>Comments</span></strong><span> section.</span></p></div>";
+        public List<string> Tests { get; set; } = new string[] { "Test1", "Test2" }.ToList();
+
         private Dictionary<string, string> _tags = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Dictionary<string, string> Tags { get => _tags; set => _tags = value; }
     }
